816eac6ff20d0af17c657242078347624c32f50c
cpereiramt <cpereiramt@gmail.com>
2019-05-30 20:16:47 -0400

    subindo os ultimos arquivos de configuração reactjs e iniciando a implementação dos testes das router

--- backend/routes/Routes_test.go
+++ backend/routes/Routes_test.go
@@ -1,3 +1,33 @@
 package routes
 
-//import ("github.com/stretchr/testify/assert")
+import ("github.com/stretchr/testify/assert"
+         "testing"
+			"net/http" 
+			 "net/http/httptest"
+			"github.com/gin-gonic/gin")
+
+
+
+	func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+				req, _ := http.NewRequest(method, path, nil)
+				w := httptest.NewRecorder()
+				r.ServeHTTP(w, req)
+				return w
+			 }
+
+func TestStartRouter(t *testing.T) {
+	// Assert we encoded correctly,
+   // the request gives a 200
+    // Perform a GET request with that handler.
+	router := gin.Default()
+
+	w := performRequest(router, "GET", "")
+	assert.Equal(t, http.StatusOK, w.Code)
+	w  = performRequest(router, "POST", "/api/v1/signin")
+	assert.Equal(t, http.StatusOK, w.Code)
+	w  = performRequest(router, "POST", "/api/v1/signup")
+	assert.Equal(t, http.StatusOK, w.Code)
+
+  
+
+	}
