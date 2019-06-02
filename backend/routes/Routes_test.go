9a6bdde81f49e719d3b28b7deaf76edebd44e53e
cpereiramt <cpereiramt@gmail.com>
2019-06-02 09:50:10 -0400

    retornando codigo 401, porem ainda não testando autenticação corretamente.

--- backend/routes/Routes_test.go
+++ backend/routes/Routes_test.go
@@ -1,46 +1,44 @@
- package routes
+package routes
 
-import ("github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 
-         "testing"
-			"net/http"
-			 "net/http/httptest"
-			//"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-		)
+func performRequest(r http.Handler, method, path string, t *testing.T) *httptest.ResponseRecorder {
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJ1aWJsYWVzZUBnbWFpbC5jb20iLCJleHAiOjE1NTk0MjQxMjIsIm9yaWdfaWF0IjoxNTU5NDIwNTIyfQ.kdIRkLjRc63VQvDcHECId45_8rlCr8QlAmVBcEG2tlE"
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
+	r.ServeHTTP(w, req)
 
-
-
-	func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
-				req, _ := http.NewRequest(method, path, nil)
-				w := httptest.NewRecorder()
-				r.ServeHTTP(w, req)
-				return w
-			 }
-
+	assert.Equal(t, w.Code, http.StatusOK)
+	return w
+}
 
 func TestStartRouter(t *testing.T) {
 	// Assert we encoded correctly,
-   // the request gives a 200
-    // Perform a GET request with that handler.
+	// the request gives a 200
+	// Perform a GET request with that handler.
 	router := StartRouter()
 
-	w := performRequest(router, "GET", "/")
-
-
+	w := performRequest(router, "GET", "/",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w  = performRequest(router, "POST", "/api/v1/signin")
+	w = performRequest(router, "POST", "/api/v1/signin",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w  = performRequest(router, "POST", "/api/v1/signup")
+	w = performRequest(router, "POST", "/api/v1/signup",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w = performRequest(router, "GET", "/user")
+	w = performRequest(router, "GET", "/user",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w = performRequest(router, "GET", "/user/id")
+	w = performRequest(router, "GET", "/user/id",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w = performRequest(router, "GET", "/customer")
+	w = performRequest(router, "GET", "/customer",t)
 	assert.Equal(t, http.StatusOK, w.Code)
-	w = performRequest(router, "GET", "/customer/id")
+	w = performRequest(router, "GET", "/customer/id",t)
 	assert.Equal(t, http.StatusOK, w.Code)
 
-
-	} 
+}
