1a3fd220dc80baee89a135c9493d605300866524
cpereiramt <cpereiramt@gmail.com>
2019-06-06 13:05:03 -0400

    criando autenticação e tela de login

--- frontend/src/services/Auth/JWTAuth.js
+++ frontend/src/services/Auth/JWTAuth.js
@@ -11,16 +11,37 @@ let response = await axios.post(LOGIN_ENDPOINT, data);
 if (response.status === 200 && response.data.jwt && response.data.expireAt){
 
 let jwt = response.data.jwt;
-let expire_at = response.data.expireAt
+let expire_at = response.data.expireAt;
+
+localStorage.setItem("acess_token", jwt);
+localStorage.setItem("expires_at", expire_at);
 
 
 }
-
-
-
-
-}catch(e){
-
+}
+catch(e){
+console.log(e);
 
 }
+}
+
+const register = async(data)=>{
+  const SIGNUP_ENDEPOINT = `${SERVER_URL}/api/v1/signup`;
+  
+    try{
+        let response = await axios({
+          method :'post',
+          responseType: 'json',
+          url : SIGNUP_ENDEPOINT,
+          data: data
+        });
+
+     }catch(e){
+    console.log(e);
+
+     }
+
+
+
+
 }
