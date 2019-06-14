4df1e0e33e0517dd4c008bbc8d65ffe0e17da5a9
Rui Richard Blaese <ruiblaese@gmail.com>
2019-06-14 19:53:29 -0300

    ajuste html login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router";
-import PropTypes from "prop-types";
+import React, {Component} from 'react';
+import {withRouter} from 'react-router';
+import PropTypes from 'prop-types';
 
-import loginService from "../services/loginService";
+import loginService from '../services/loginService';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: ""
+      username: '',
+      password: '',
     };
   }
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({[event.target.name]: event.target.value});
   }
   async login(event) {
     event.preventDefault();
 
     try {
       await loginService.login(this.state.username, this.state.password);
-      this.props.history.push("/");
+      this.props.history.push('/');
     } catch (error) {
       event.preventDefault();
       alert(error);
@@ -28,56 +28,59 @@ class Login extends Component {
   }
 
   render = () => (
-    <form className="form-signin">
+    <div className="container">
+      <br />
       <div className="text-center mb-4">
-        <h1 className="h3 mb-3 font-weight-normal">Login / Register</h1>
+        <h1 className="h3 mb-3 font-weight-normal">Login</h1>
       </div>
 
-      <div className="form-label-group">
-        <label htmlFor="inputEmail">Username</label>
-        <input
-          name="username"
-          onChange={e => {
-            this.handleChange(e);
-          }}
-          value={this.state.username}
-          className="form-control"
-          placeholder="Username"
-          required
-        />
-      </div>
+      <form className="form-signin">
+        <div className="form-label-group">
+          <label htmlFor="inputEmail">Username</label>
+          <input
+            name="username"
+            onChange={e => {
+              this.handleChange(e);
+            }}
+            value={this.state.username}
+            className="form-control"
+            placeholder="Username"
+            required
+          />
 
-      <div className="form-label-group mt-2">
-        <label htmlFor="inputPassword">Password</label>
-        <input
-          name="password"
-          onChange={e => {
-            this.handleChange(e);
-          }}
-          value={this.state.password}
-          type="password"
-          className="form-control"
-          placeholder="Password"
-          required
-        />
-      </div>
+          <div className="form-label-group mt-2">
+            <label htmlFor="inputPassword">Password</label>
+            <input
+              name="password"
+              onChange={e => {
+                this.handleChange(e);
+              }}
+              value={this.state.password}
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
 
-      <div className="mt-5">
-        <button
-          type="submit"
-          onClick={e => {
-            this.login(e);
-          }}
-          className="login btn btn-lg btn-primary btn-block"
-        >
-          Login
-        </button>
-      </div>
-    </form>
+          <div className="mt-5">
+            <button
+              type="submit"
+              onClick={e => {
+                this.login(e);
+              }}
+              className="login btn btn-lg btn-primary btn-block"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
   static propTypes = {
     location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
+    history: PropTypes.object.isRequired,
   };
 }
 
