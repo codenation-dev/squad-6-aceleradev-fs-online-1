2016ddd13c23088c3c6e7132d01cbbe3f8d04f1c
Rui Richard Blaese <ruiblaese@gmail.com>
2019-06-15 18:02:43 -0300

    iniciando paginas do cliente

--- frontend/src/services/loginService.js
+++ frontend/src/services/loginService.js
@@ -1,23 +1,23 @@
-import { localStorageWrapper } from "../helpers";
+import {localStorageWrapper} from '../helpers';
 
-const axios = require("axios");
+const axios = require('axios');
 
-const NS_LOGGED_USER = "logged_user";
+const NS_LOGGED_USER = 'logged_user';
 
-const { SIGNUP_ENDPOINT } = require("./configApi");
+const {SIGNUP_ENDPOINT} = require('./configApi');
 
 async function login(username, password) {
   const configRequest = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json',
     },
     json: true,
     url: SIGNUP_ENDPOINT,
     data: JSON.stringify({
       email: username,
-      password: password
-    })
+      password: password,
+    }),
   };
   //efetua requisicao em si
   const response = await axios(configRequest);
@@ -27,17 +27,17 @@ async function login(username, password) {
       email: username,
       password: password,
       token: response.data.token,
-      expire: response.data.expire
+      expire: response.data.expire,
     });
     return true;
   }
   return false;
 }
 
-export const isLogged = () => !!localStorageWrapper.get(NS_LOGGED_USER);
+const isLogged = () => !!localStorageWrapper.get(NS_LOGGED_USER);
 
-export const userLogged = () => localStorageWrapper.get(NS_LOGGED_USER);
+const userLogged = () => localStorageWrapper.get(NS_LOGGED_USER);
 
-export const logout = () => localStorageWrapper.set(NS_LOGGED_USER, null);
+const logout = () => localStorageWrapper.set(NS_LOGGED_USER, null);
 
-export default { login, logout, isLogged, userLogged };
+export default {login, logout, isLogged, userLogged};
