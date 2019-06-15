4b2c4a51479f5731a245b4a171cdd7209d28268d
cpereiramt <cpereiramt@gmail.com>
2019-06-15 12:43:34 -0400

    refatorando o component

--- frontend/src/components/UploadCSV.js
+++ frontend/src/components/UploadCSV.js
@@ -1,4 +1,3 @@
-import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import React, {Component} from "react";
 import {withRouter} from 'react-router';
@@ -7,21 +6,49 @@ class UploadCSV extends Component {
     constructor(props) {
       super(props);
       this.state = {
+        file:null
      
       };
     }
- 
-
-
   
+    handleFIle(e){
+      let file
+      this.setState({file:file})
+/* 
+console.log(e.target.files)
+console.log(e.target.files[0]) */
+
+
+    }
    
+
+     handleUpload(e){
+
+     console.log()
+
+
+     }
     render = () => (
         <div className="container">
+        <form>
+         <div className="">
+          <label>Selecione o arquivo</label>
+           <input type="file" name="file" onChange={(e) => 
+            this.handleFIle(e)}/>
+
+        <button type="button" 
+         onClick={(e)=>this.handleUpload(e)}
+        >Upload </button>
+         </div>
+    
+
+
+        </form>
        
-       <FilePond allowMultiple={false} 
-       maxFiles={1} server='http://localhost:4000/api/v1/customer/upload'
-       type/>
-        </div>
+                   
+ 
+ 
+ </div>
   
 
   
