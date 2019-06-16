66eb1232ce00097c1b65a60eaf33352b99325ad3
cpereiramt <cpereiramt@gmail.com>
2019-06-16 15:35:30 -0400

    alterando tela de upload de CSV

--- frontend/src/components/UploadCSV.js
+++ frontend/src/components/UploadCSV.js
@@ -24,47 +24,49 @@ class UploadCSV extends Component {
 
   render = () => (
     <div className="container">
-      <div className="row">
-        <h3>Upload de Clientes por arquivo CSV</h3>
-        <br />
-        <br />
-      </div>
-      <div className="row">
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <button className="btn btn-outline-secondary" type="button">
-              Escolher Arquivo
-            </button>
-          </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder=""
-            aria-label=""
-            aria-describedby="basic-addon1"
-          />
-          <div className="input-group-append">
+    <div className="row">
+    <h3>Upload de Clientes por arquivo CSV</h3>
+    <br />
+    <br />
+  </div>
+ 
+ 
+            <div className="row">
+<div class="input-group mb-3">
+<div class="input-group-prepend">
+<button class="btn btn-outline-secondary"
+ type="button">Escolher Arquivo</button>
+</div>
+<div class="custom-file">
+ <input type="file" class='custom-file-input'
+ id='inputGroupFile03' onChange={e => this.handleFIle(e)}
+/>
+  <label class="custom-file-label" 
+  for="inputGroupFile03"  value={this.setState.file} >Choose file</label>
+  
+</div>
+<div className="input-group-append">
             <button className="btn btn-outline-secondary" type="button">
               Limpar
             </button>
-            <button className="btn btn-outline-primary" type="button">
+            <button className="btn btn-outline-primary" type="button" onClick={e => this.handleUpload(e)}>
               Fazer Upload
             </button>
           </div>
-        </div>
+</div>
+</div>
+  
 
-        <form>
-          <div className="">
-            <label>Selecione o arquivo</label>
-            <input type="file" name="file" onChange={e => this.handleFIle(e)} />
+   
+    
 
-            <button type="submit" onClick={e => this.handleUpload(e)}>
-              Upload
-            </button>
-          </div>
-        </form>
+     
+
+        
+
+        
       </div>
-    </div>
+   
   );
 }
 export default withRouter(UploadCSV);
