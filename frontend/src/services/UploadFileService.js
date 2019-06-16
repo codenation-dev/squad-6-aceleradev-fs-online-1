1d891ed634607e4a5013d84baef6ea96f77fcd9c
cpereiramt <cpereiramt@gmail.com>
2019-06-15 23:10:03 -0400

    Upload funcionando 1

--- frontend/src/services/UploadFileService.js
+++ frontend/src/services/UploadFileService.js
@@ -1,5 +1,5 @@
 import loginService from "./loginService";
-
+import { localStorageWrapper } from "../helpers";
 const { CSV_FILE_UPLOAD } = require("./configApi");
 
 const axios = require("axios");
@@ -7,18 +7,25 @@ const axios = require("axios");
 async function uploadFile(file) {
 
     let files = file
+    console.log(files, "dentro da função de upload!")
     let formdata = new FormData()
-  formdata.append('multipart/form-data', files)
+  formdata.append('file', files)
   const configRequest = {
     method: "POST",
-    json: true,
-    headers: {
+      headers: {
       Authorization: "Bearer " + loginService.userLogged().token
     },
     url: CSV_FILE_UPLOAD,
     data: formdata
   };
   //efetua requisicao em si
+  console.log(formdata,'arquivo formdata')
+  console.log("Bearer " + loginService.userLogged().token, "token from login !")
+  console.log("Bearer " + localStorageWrapper.get("logged_user").token, "token from storage !")
+  console.log(configRequest,"here request")
+
+  
+ 
   const response = await axios(configRequest);
 
   if (response) {
