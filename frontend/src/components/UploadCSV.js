3ce580f2627d7d9ef940b0a93328744be631b75e
cpereiramt <cpereiramt@gmail.com>
2019-06-17 15:27:30 -0400

    criando css para o component uploadCSV

--- frontend/src/components/UploadCSV.js
+++ frontend/src/components/UploadCSV.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router';
 
 import fileUploadCSV from './../services/UploadFileService';
-
+import '../components/UploadCSV.css'
 class UploadCSV extends Component {
   constructor(props) {
     super(props);
