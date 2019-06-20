244cbe7b8c56d3a87154fcfb1c0bcd07153d7d34
cpereiramt <cpereiramt@gmail.com>
2019-06-19 20:03:11 -0400

    configurando a pesquisa na tela Alertas

--- frontend/src/components/Alerts.js
+++ frontend/src/components/Alerts.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 
 import alertService from '../services/alertService';
@@ -7,35 +7,60 @@ import alertService from '../services/alertService';
 class Alerts extends Component {
   constructor(props) {
     super(props);
-    this.state = {listAlerts: [],
+    this.state = {
+      listAlerts: [],
       searchString: ''
     };
-   this.pegaValor = this.pegaValor.bind(this)
-   this.testa_valor= this.testa_valor.bind(this)
+
   }
   async componentDidMount() {
     const retorno = await alertService.getAlerts();
-    console.log(retorno,"Alert retorno")
+    console.log(retorno, "Alert retorno")
     this.setState({
-      listAlerts: retorno,
+      listAlerts: [retorno[0]],
       searchString: '',
     });
   }
 
- pegaValor(e){
-  console.log(this.state.searchString)
-  this.setState({searchString:e.target.value})
+  filtraValor(e) {
+    // Variable to hold the original version of the list
+    let currentList = [];
+    // Variable to hold the filtered list before putting into state
+    let newList = [];
+
+    
+    if (e.target.value !== "") {
+      if(this.state.listAlerts){
+   
+      currentList = this.state.listAlerts
+      console.log(currentList,"alertas")
+      }
+   
+      newList = currentList.filter(item => {
+       
+        const lc = item.toLowerCase();
+       
+        const filter = e.target.value.toLowerCase();
+       
+        return lc.includes(filter);
+      });
+    } else {
+ 
+      newList = this.props.items;
+    }
+    
+    this.setState({
+      listAlerts: newList
+    });
   }
 
 
-  testa_valor(e){
 
-console.log(this.state.searchString,"testando a leitura dos valores !")
 
-  }
+
   btnNewClick(event) {
     this.props.history.push('/alert/');
-    
+
   }
 
   btnEditClick(event, item) {
@@ -48,19 +73,19 @@ console.log(this.state.searchString,"testando a leitura dos valores !")
     this.setState({
       listAlert: retorno,
     });
-   }
+  }
 
- 
+
   render = () => (
     <React.Fragment>
       <div className="container">
         <div className="row">
-    <input type="text"
-     name="pesquisa"
-     onChange={(e)=>this.pegaValor(e)} />
+          <input type="text"
+            name="pesquisa"
+            onChange={(e) => this.filtraValor(e)} />
 
 
-    <button onClick={(e)=>this.testa_valor(e)}>  pesquisar </button>
+          <button >  pesquisar </button>
 
         </div>
         <br />
@@ -79,25 +104,25 @@ console.log(this.state.searchString,"testando a leitura dos valores !")
               {this.state.listAlerts
                 ? this.state.listAlerts.map((item, index) => (
 
-                    <tr key={item.id}>
-                      <th scope="row">{item.id}</th>
-                      <td>{item.filename}</td>
-                      <td>{item.month}</td>
-                      <td>{item.year}</td>
-                      <td>{item.EmployeePayments}</td>
-                      
-                      <td>
-                        <input
-                          onClick={e => {
-                            this.btnEditClick(e, item);
-                          }}
-                          className="btn btn-primary btn-sm"
-                          type="button"
-                          value="visualizar"
-                          name="btnEdit"
-                          id="btnEdit"
-                        />
-                    {/*     <input
+                  <tr key={item.id}>
+                    <th scope="row">{item.id}</th>
+                    <td>{item.filename}</td>
+                    <td>{item.month}</td>
+                    <td>{item.year}</td>
+                    <td>{item.EmployeePayments}</td>
+
+                    <td>
+                      <input
+                        onClick={e => {
+                          this.btnEditClick(e, item);
+                        }}
+                        className="btn btn-primary btn-sm"
+                        type="button"
+                        value="visualizar"
+                        name="btnEdit"
+                        id="btnEdit"
+                      />
+                      {/*     <input
                           onClick={e => {
                             this.btnDeleteClick(e, item);
                           }}
@@ -107,9 +132,9 @@ console.log(this.state.searchString,"testando a leitura dos valores !")
                           name="btnDelete"
                           id="btnDelete"
                         /> */}
-                      </td>
-                    </tr>
-                  ))
+                    </td>
+                  </tr>
+                ))
                 : ''}
             </tbody>
           </table>
